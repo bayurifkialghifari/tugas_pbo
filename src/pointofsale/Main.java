e7cc35802806c469f17a9bb4c7c892772cb11142
Bayu Rifki Alghifari <50207539+BayuRifkiAlghifari@users.noreply.github.com>
2022-01-18 08:23:34 +0700

    Add label to main menu before login

--- src/pointofsale/Main.java
+++ src/pointofsale/Main.java
@@ -1,6 +1,7 @@
 package pointofsale;
 
 import java.lang.reflect.*;
+import java.util.Arrays;
 import java.util.Scanner;
 import javax.sql.rowset.CachedRowSet;
 import middleware.authentication;
@@ -24,20 +25,43 @@ public class Main {
         // CachedRowSet
         CachedRowSet crs;
         
+        // Menu list now        
+        int i;
+        int parent_menu;
+        int select_menu;
+        String menu_list [] = new String [101];       
+        
         do
         {
             if(!auth.isLoggedIn)
             {
                 // Show login and register menu                
-                crs = menu.select_where("*", "menu_menu_id", "0", "");
+                crs = menu.select_where("*", "menu_menu_id", "0", "and menu_class != 'home'");
+                
+                i = 0;
+                
+                // Label
+                System.out.println("Silakan pilih salah satu menu yang ada di bawah ini");
                 
                 while(crs.next())
                 {
-                    if(!crs.getString("menu_class").equals("home"))
-                        System.out.println(crs.getString("menu_index") + " " + crs.getString("menu_name"));
+                    menu_list[i+1] = crs.getString("menu_name");
+                    i++;
+                    
+                    System.out.println(crs.getString("menu_index") + ". " + crs.getString("menu_name"));
                 }
                 
-                input.nextInt();
+                System.out.println(i+1 + ". Keluar \n");
+                
+                System.out.println("Pilihan anda ?");
+                
+                select_menu = input.nextInt();
+                
+                // Check exit
+                exit = select_menu == (i+1) ? true : false;
+                
+                if(!exit)
+                    System.out.println(menu_list[select_menu]);
             }
         }
         while(!exit);
@@ -49,4 +73,5 @@ public class Main {
 //        method.invoke(instance);
         
     } 
+
 }
